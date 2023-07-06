190fb54e08685327c2fcb6167b8a0b0334d348a4
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 16:05:34 +0300

    create contact-page component in contact module

--- booking-app/src/app/contact/contact.module.ts
+++ booking-app/src/app/contact/contact.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactPageComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [ContactPageComponent]
 })
 export class ContactModule { }
