22362f5859d8f4a62243adbc40c4eb4476c089a0
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 14:34:42 +0300

    make video-section home component

--- booking-app/src/app/home/home.module.ts
+++ booking-app/src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AboutModule } from '../about/about.module';
+import { VideoSectionComponent } from './video-section/video-section.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    VideoSectionComponent
   ],
   imports: [
     CommonModule, AboutModule
