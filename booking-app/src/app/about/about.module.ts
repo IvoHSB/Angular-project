02a0e9127bc595be07a540af5fc69a26902706fe
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 14:26:03 +0300

    make about-page component in about module

--- booking-app/src/app/about/about.module.ts
+++ booking-app/src/app/about/about.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsSectionComponent } from './about-us-section/about-us-section.component';
 import { OutTeamSectionComponent } from './out-team-section/out-team-section.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 
 
 @NgModule({
   declarations: [
     AboutUsSectionComponent,
-    OutTeamSectionComponent
+    OutTeamSectionComponent,
+    AboutPageComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [AboutUsSectionComponent, OutTeamSectionComponent]
+  exports: [AboutUsSectionComponent, OutTeamSectionComponent, AboutPageComponent]
 })
 export class AboutModule { }
