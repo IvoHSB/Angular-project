750cd1ae378e874ec3c53eae9c936356596832da
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 13:36:17 +0300

    make booking core component

--- booking-app/src/app/core/core.module.ts
+++ booking-app/src/app/core/core.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './header/header.component';
+import { BookingComponent } from './booking/booking.component';
 
 
 
 @NgModule({
   declarations: [
     NavigationComponent,
-    HeaderComponent
+    HeaderComponent,
+    BookingComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [NavigationComponent, HeaderComponent]
+  exports: [NavigationComponent, HeaderComponent, BookingComponent]
 })
 export class CoreModule { }
