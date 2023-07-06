083fd31071e7ee5d1b6ee47e44277ae9a7bb0649
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 16:10:29 +0300

    make header contact component

--- booking-app/src/app/contact/contact.module.ts
+++ booking-app/src/app/contact/contact.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    ContactPageComponent
+    ContactPageComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
