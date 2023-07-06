c5d0a21ad9527d7a744fcf4534e0a9c64c12973a
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 14:45:26 +0300

    recreate out-services-section services component

---
+++ booking-app/src/app/services/our-services-section/our-services-section.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OurServicesSectionComponent } from './our-services-section.component';
+
+describe('OurServicesSectionComponent', () => {
+  let component: OurServicesSectionComponent;
+  let fixture: ComponentFixture<OurServicesSectionComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [OurServicesSectionComponent]
+    });
+    fixture = TestBed.createComponent(OurServicesSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- booking-app/src/app/services/services.module.ts
+++ booking-app/src/app/services/services.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OurServicesSectionComponent } from './our-services-section/our-services-section.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OurServicesSectionComponent
+  ],
   imports: [
     CommonModule
   ]
