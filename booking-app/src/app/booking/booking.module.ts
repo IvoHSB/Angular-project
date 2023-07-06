39ecfebf939a98715f2aa5aa22027f805cd114fe
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 15:16:02 +0300

    make booking-page component in booking module

--- booking-app/src/app/booking/booking.module.ts
+++ booking-app/src/app/booking/booking.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OurOffersSectionComponent } from './our-offers-section/our-offers-section.component';
+import { BookingPageComponent } from './booking-page/booking-page.component';
 
 
 
 @NgModule({
   declarations: [
-    OurOffersSectionComponent
+    OurOffersSectionComponent,
+    BookingPageComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [BookingPageComponent]
 })
 export class BookingModule { }
