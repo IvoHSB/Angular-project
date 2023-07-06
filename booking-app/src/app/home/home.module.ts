b33bd691fd877c4404c2034b777a205eaa619f46
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 14:10:30 +0300

    make home-page home component and add about-us-section about component in him

--- booking-app/src/app/home/home.module.ts
+++ booking-app/src/app/home/home.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutModule } from '../about/about.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule, AboutModule
+  ],
+  exports: [HomePageComponent]
 })
 export class HomeModule { }
