f79aabc3d4c869f9dc8f46c13a0634afba7be5ef
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 14:38:50 +0300

    create service module

--- booking-app/src/app/app.module.ts
+++ booking-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CoreModule } from './core/core.module';
 import { ShareModule } from './share/share.module';
 import { HomeModule } from './home/home.module';
 import { AboutModule } from './about/about.module';
+import { ServiceModule } from './service/service.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AboutModule } from './about/about.module';
     CoreModule,
     ShareModule,
     HomeModule,
-    AboutModule
+    AboutModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
