296b87a2659a6e78e868f4cad6620d890cbfb312
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-06 16:14:22 +0300

    make map, contact's component

--- booking-app/src/app/contact/contact.module.ts
+++ booking-app/src/app/contact/contact.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { HeaderComponent } from './header/header.component';
 import { EmailsSectionComponent } from './emails-section/emails-section.component';
+import { MapComponent } from './map/map.component';
 
 
 
@@ -10,7 +11,8 @@ import { EmailsSectionComponent } from './emails-section/emails-section.componen
   declarations: [
     ContactPageComponent,
     HeaderComponent,
-    EmailsSectionComponent
+    EmailsSectionComponent,
+    MapComponent
   ],
   imports: [
     CommonModule
