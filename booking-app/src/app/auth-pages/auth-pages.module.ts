47e9f8e5078b4f814ef4558dbf87e8b62b034de8
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-07 09:44:59 +0300

    make login-page, auth-page's component

--- booking-app/src/app/auth-pages/auth-pages.module.ts
+++ booking-app/src/app/auth-pages/auth-pages.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 
 @NgModule({
   declarations: [
-    RegisterPageComponent
+    RegisterPageComponent,
+    LoginPageComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [RegisterPageComponent]
+  exports: [RegisterPageComponent, LoginPageComponent]
 })
 export class AuthPagesModule { }
