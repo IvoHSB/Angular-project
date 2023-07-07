5be451ccc9685dd52b0e71ab79d66197ead6d13e
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-07 09:26:14 +0300

    make our-team-page, our-team's component

--- booking-app/src/app/our-team/our-team.module.ts
+++ booking-app/src/app/our-team/our-team.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OurTeamPageComponent } from './our-team-page/our-team-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OurTeamPageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    OurTeamPageComponent
   ]
 })
 export class OurTeamModule { }
