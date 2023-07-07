7db8707c38d314b2d9a0db1265f80886c0bcd2d7
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-07-07 12:57:21 +0300

    make header and navigation change on separate page (not reactivity)

--- booking-app/src/app/core/navigation/navigation.component.ts
+++ booking-app/src/app/core/navigation/navigation.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  activePage = "home"
 
+  constructor(private location: Location) {}
+
+  ngOnInit(): void {
+    if (this.location.path() === '') {
+      this.activePage = 'home';
+    } else {
+      this.activePage = this.location.path().split('/')[1];
+    }
+
+  }
 }
