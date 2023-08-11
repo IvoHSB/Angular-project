d9778f1b305cb0fe9e8fef466e71faf9134b1465
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-08-12 01:58:59 +0300

    fix google maps but don't have api key

--- booking-app/src/app/contact/map/map.component.ts
+++ booking-app/src/app/contact/map/map.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, Renderer2, ViewChild } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  @ViewChild('mapContainer') mapContainer: ElementRef;
 
+  constructor(private renderer: Renderer2, private ngZone: NgZone) {}
+
+  ngOnInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      const map = new google.maps.Map(this.mapContainer.nativeElement, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      });
+
+      // Add more map-related code and interactions here
+    });
+  }
 }
